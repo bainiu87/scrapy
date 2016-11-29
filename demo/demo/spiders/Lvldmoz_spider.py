4a16d77d2e2c4f8d4dc2ec80b82a46ed42c6ffe8
baiyun <baiyun5522359@163.com>
2016-11-29 12:01:28 +0800

    'demo'

---
+++ demo/demo/spiders/Lvldmoz_spider.py
@@ -0,0 +1,27 @@
+import scrapy
+from scrapy.selector import Selector
+from scrapy.http import HtmlResponse
+from scrapy.http import Request
+from demo.items import LvlItem
+from scrapy.contrib.linkextractors import LinkExtractor
+class LvlitemSpider(scrapy.Spider):
+    name="lvl"
+    allowed_domains = ["ll.gov.cn"]
+    start_urls=[
+        "http://www.ll.gov.cn/lvliang/"
+    ]
+    def parse(self, response):
+        item = LvlItem()
+        a=[]
+        for sel in response.xpath('//ul[@class="e2"]'):
+                item['uu']=response.url
+                item['title']=sel.xpath(".//li/a/text()").extract()
+                item['link']=sel.xpath(".//li/a@href").extract()
+                yield item
+        nextlin=response.xpath("//ul[@class='pagelist']/li/a/@href").extract()
+        if nextlin:
+            for i in xrange(1,10):
+                yield Request(response.urljoin(nextlin[i]),callback=self.parse)
+
+
+
